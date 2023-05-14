8906cb377b9642fee694f709ed1df1b2d710e2d3
Anton <antony.adamson@gmail.com>
2023-05-14 16:32:39 +0600

    Start to Finish v2

--- DC-Lab6-Contest/ViewController.swift
+++ DC-Lab6-Contest/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        if emailTextField.text?.isEmpty == false {
+            performSegue(withIdentifier: "ShowResult", sender: nil)
+        } else {
+            UIView.animate(withDuration: 0.2, animations: {
+                let rigth = CGAffineTransform(translationX: 20, y: 0)
+                self.emailTextField.transform = rigth
+            }) { (_) in
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.emailTextField.transform = CGAffineTransform.identity
+                })
+            }
+        }
+    }
+    
 }
 
