a843192f49ca383f299d781d0e8eb819dc3956eb
Anton <antony.adamson@gmail.com>
2023-05-14 16:18:02 +0600

    Initial Commit

---
+++ DC-Lab6-Contest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DC-Lab6-Contest
+//
+//  Created by Антон Адамсон on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
